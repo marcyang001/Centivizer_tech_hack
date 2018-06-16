76e0ca2f06a5a90f7ea23098a5ed68eae750cc0b
Marc Yang <m.y.yang001@gmail.com>
2018-06-16 12:14:58 -0400

    fix the extension problem

--- routes/form.js
+++ routes/form.js
@@ -2,15 +2,26 @@ var express = 	require('express');
 var router 	= 	express.Router();
 var multer	=	require('multer');
 
+var storage	=	multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, './public/images');
+  },
+  filename: function (req, file, callback) {
+    callback(null, file.originalname);
+  }
+});
 
-/* GET users listing. */
+var upload = multer({ storage : storage});
+
+
+/* GET form. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
   res.status(200);
 });
 
 router.post('/generate', function(req, res, next) {
-
+	
 	
 
 
--- routes/interact.js
+++ routes/interact.js
@@ -1,2 +1,11 @@
 var express = 	require('express');
 var router 	= 	express.Router();
+
+
+/* GET interaction game. */
+router.get('/', function(req, res, next) {
+  res.send('respond with interaction');
+  res.status(200);
+});
+
+module.exports = router;
