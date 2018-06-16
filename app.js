e08a2321a3d5d3e54a0c0bd82ef7205e24b5e378
Marc Yang <m.y.yang001@gmail.com>
2018-06-16 11:36:50 -0400

    add interact url extension

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var form = require('./routes/form');
+var interact = require('./routes/interact');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/form', form);
+app.use('/interact', interact);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/form.js
+++ routes/form.js
@@ -1,10 +1,20 @@
-var express = require('express');
-var router = express.Router();
+var express = 	require('express');
+var router 	= 	express.Router();
+var multer	=	require('multer');
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
   res.status(200);
 });
+
+router.post('/generate', function(req, res, next) {
+
+	
+
+
+});
+
 
 module.exports = router;
