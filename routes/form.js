607d77c40ce28f6eb279fb615982b292360da474
Marc Yang <m.y.yang001@gmail.com>
2018-06-16 11:06:23 -0400

    change the name of extension

--- routes/form.js
+++ routes/form.js
@@ -3,7 +3,6 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  console.log("enter here");
   res.send('respond with a resource');
   res.status(200);
 });
