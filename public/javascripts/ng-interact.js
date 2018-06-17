5096b8db0a478fa86127d461a44949f76253d3c8
Ryan Duan <Ryan.R.Duan@ibm.com>
2018-06-17 11:54:44 -0400

    get question from Node.js server on ng-control load

--- public/javascripts/ng-interact.js
+++ public/javascripts/ng-interact.js
@@ -1,6 +1,16 @@
 var app = angular.module('memory', []);
-app.controller('myCtrl', function($scope) {
-    $scope.answers = ["Bob", "Alice", "Darren", "Marc"];
-    $scope.imageName = 'pic2.jpg';
-    $scope.question = "Question: What's your Name?";
+app.controller('myCtrl', function($scope, $http) {
+    $scope.loadData = function() {
+	$http.get('/interact').then(function(response){
+	    console.log(response.data);
+            var questionDisp = response.data;
+	    $scope.imageName = questionDisp.imageName;
+	    $scope.question = questionDisp.question;
+	    $scope.answers = questionDisp.wrongAnswers;
+	    $scope.answers.push(questionDisp.rightAnswer);
+	    $scope.answers.sort();
+	    $scope.rightAnswer = questionDisp.rightAnswer;
+        })
+    }
+    $scope.loadData();
 });
