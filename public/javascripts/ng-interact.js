1b986c6e23e5cf1275d086f9012353840b117bd7
Ryan Duan <Ryan.R.Duan@ibm.com>
2018-06-17 11:15:13 -0400

    use angular variables in view

--- public/javascripts/ng-interact.js
+++ public/javascripts/ng-interact.js
@@ -1,5 +1,6 @@
 var app = angular.module('memory', []);
 app.controller('myCtrl', function($scope) {
-    $scope.firstName = "Bob";
-    $scope.imageName = "pic2.jpg";
+    $scope.answers = ["Bob", "Alice", "Darren", "Marc"];
+    $scope.imageName = 'pic2.jpg';
+    $scope.question = "Question: What's your Name?";
 });
