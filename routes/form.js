4ce91bbef32583e282c2cfda001d961902223809
Marc Yang <m.y.yang001@gmail.com>
2018-07-19 02:11:50 -0400

    parse the customized questions with their right answers and deceptors and store them into mongodb

--- routes/form.js
+++ routes/form.js
@@ -109,6 +109,14 @@ router.post('/generate', function(req, res, next) {
 
 });
 
+/*
+	The user uploads a CSV file 
+	
+	The handler will read the csv file and parse each line into 
+	json object (imageInfo), then generate to a set of questions from 
+	the json object imageInfo
+	
+*/
 
 router.post('/uploadcsv', function(req, res, next) {
 
@@ -135,19 +143,39 @@ router.post('/uploadcsv', function(req, res, next) {
     			
     			// parse into a json object and store into mongoDB
     			var imageInfo = {};
-
+				
+				/*
+				    			{
+				  imageUrl: "http://localhost:3000/images/imageName.jpg/",
+				  who: "Alice",
+				  where: "Paris",
+				  year: 1998,
+				  month: 7,
+				  questions: [
+				    { 
+				      q1: "Question 1",
+				      rightAnswer: "right answer",
+				      wrongAnswers: [ "a1", "a2", "a3" ]
+				    },
+				    { 
+				      q2: "Question 2",
+				      rightAnswer: "right answer",
+				      wrongAnswers: [ "a1", "a2", "a3" ]
+				    }
+				  ]
+				}
+				   */ 				
     			imageInfo["imageUrl"] = csvRow["imageUrl"];
     			imageInfo["who"] = csvRow["who"];
     			imageInfo["where"] = csvRow["where"];
     			imageInfo["year"] = csvRow["year"];
     			imageInfo["month"] = csvRow["month"];
 
+    			// customized questions added by the user 
+    			imageInfo["questions"] = parseCustomizedQuestionList(csvRow);
+    			
 
-    			/* TODO for customized questions */ 
-    			//								 //
-    			//								 //
-    			//								 //
-    			///////////////////////////////////
+    			console.log(imageInfo);
 
     			const collectionPics = db.get(picCollection);
 				const collectionQBank = db.get(questionBankCollection);
@@ -196,17 +224,62 @@ router.post('/uploadcsv', function(req, res, next) {
 });
 
 
+function parseCustomizedQuestionList(csvRow) {
+
+	var questionList = [];
+
+	// regular expression for question## header in csv
+	var re_question = new RegExp("^(question)[0-9]+$");
+
+	for (var field in csvRow) {
+
+		
+
+		// if we find the "question##" header
+		if (re_question.test(field) && csvRow[field] !== "") {
+			// extract the number, the string "question" contains 8 characters,
+			// so the number must start with the 8th character
+			var questionNum = parseInt(field.substring(8));
+			
+			// then we can index the answer headers based on the pattern: 
+			// q##_right and q##_wrong## 
+			var customizedQuestion = {};
+
+			// fields for customized question {q1: ..., rightAnswers : ..., wrongAnswers : [ ... ] }
+			
+			var questionNumRight = "q"+questionNum + "_right";
+			var questionNumWrong = "q"+questionNum + "_wrong";
+
+			customizedQuestion["question"] = csvRow[field];
+			customizedQuestion["rightAnswer"] = csvRow[questionNumRight];
+			// array to store all the wrong answers (deceptors) 
+			customizedQuestion["wrongAnswers"] = [];
+			// the user only need to provide 3 wrong answers (1 to 3 inclusive) in the csv 
+			// under the header q##_wrong##
+			for (var i = 1; i <= 3; i++) {
+				customizedQuestion["wrongAnswers"].push(csvRow[questionNumWrong+i]);
+			}
+
+			questionList.push(customizedQuestion);
+		}
+	}
+
+	return questionList;
+}
+
+
+
 function generateQuestionDocs(imageInfo) {
 
 	var resultQuestionDocs = [];
 	// generate questions based on image input (iterate through the keys)
 	for (var field in imageInfo) { 
 			
-		var questionDoc = {};
-
+		
 		if (field != "questions") {
-				
-			questionDoc["imageUrl"] = imageInfo["imageUrl"];
+			
+			var questionDoc = {};
+			questionDoc["imageUrl"] = imageInfo["imageUrl"];		
 			questionDoc["tag"] = field;
 
 			if (field === "who") {
@@ -234,7 +307,31 @@ function generateQuestionDocs(imageInfo) {
 		}
 		else {
 			// customized questions
-			/* TODO for later */
+			// questions: [
+			// 	    { 
+			// 	      q1: "Question 1",
+			// 	      rightAnswer: "right answer",
+			// 	      wrongAnswers: [ "a1", "a2", "a3" ]
+			// 	    },
+			// 	    { 
+			// 	      q2: "Question 2",
+			// 	      rightAnswer: "right answer",
+			// 	      wrongAnswers: [ "a1", "a2", "a3" ]
+			// 	    }
+			// 	  ]
+
+			// iterate the array
+			imageInfo[field].map(function(customQuestion) {
+
+				var questionDoc = {};
+				questionDoc["imageUrl"] = imageInfo["imageUrl"];		
+				questionDoc["tag"] = "other";
+				questionDoc["question"] = customQuestion["question"];
+				questionDoc["rightAnswer"] = customQuestion["rightAnswer"];
+				questionDoc["wrongAnswers"] = customQuestion["wrongAnswers"];
+
+				resultQuestionDocs.push(questionDoc);
+			});
 		}
 	} // end of for loop
 
